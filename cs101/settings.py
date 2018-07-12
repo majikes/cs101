85172df53dda92f2b168262f9ba9f573924c41e6
John Majikes <johnmajikes@gmail.com>
2018-07-12 02:03:18 +0000

    add database

--- cs101/settings.py
+++ cs101/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -75,8 +74,12 @@ WSGI_APPLICATION = 'cs101.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cs101',
+        'USER': 'majikesj',
+        'PASSWORD': MAJIKESJ_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
